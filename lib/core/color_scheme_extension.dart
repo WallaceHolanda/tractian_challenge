e29af0adb1c64a018fa5c4e02a6adcd8a12951d0
wallaceholanda <wallace.holanda@hotmail.com>
2024-05-29 18:48:02 -0300

    feat: adiciona assets do projeto

---
+++ lib/core/color_scheme_extension.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on ColorScheme {
+  get azulClaro => const Color.fromRGBO(33, 136, 255, 1);
+  get azulEscuro => const Color.fromRGBO(23, 25, 45, 1);
+}
